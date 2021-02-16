581c67dddfa312c1c444e7f342ce74bcf6c44655
Shawn Littrel <shawnlittrel@protonmail.com>
2021-02-15 17:40:39 -0700

    fixed bug with saving edited task.  Allow changing completion status of tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var taskIdCounter = 0;
 var pageContentEl = document.querySelector("#page-content");
+var tasksInProgressEl = document.querySelector("#tasks-in-progress");
+var tasksCompletedEl = document.querySelector("#tasks-completed");
 
 var taskFormHandler = function (event) {
   event.preventDefault();
@@ -22,9 +24,9 @@ var taskFormHandler = function (event) {
       name: taskNameInput,
       type: taskTypeInput,
     };
+
+    createTaskEl(taskDataObj);
   }
-debugger;
-  createTaskEl(taskDataObj);
 
   //check if input values are empty strings
   if (!taskNameInput || !taskTypeInput) {
@@ -39,15 +41,19 @@ debugger;
 var createTaskEl = function (taskDataObj) {
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-console.log(taskDataObj);
+  console.log(taskDataObj.name);
 
   // add task id as a custom attribute
   listItemEl.setAttribute("data-task-id", taskIdCounter);
 
   var taskInfoEl = document.createElement("div");
   taskInfoEl.className = "task-info";
-  taskInfoEl.innerHTML = "<h3 class='task-name'>" + 
-    taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+  taskInfoEl.innerHTML =
+    "<h3 class='task-name'>" +
+    taskDataObj.name +
+    "</h3><span class='task-type'>" +
+    taskDataObj.type +
+    "</span>";
   listItemEl.appendChild(taskInfoEl);
 
   var taskActionsEl = createTaskActions(taskIdCounter);
@@ -148,20 +154,44 @@ var deleteTask = function (taskId) {
   taskSelected.remove();
 };
 
-var completeEditTask = function(taskName, taskType, taskId){
-    //find matching task list item
-    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+var taskStatusChangeHandler = function (event) {
+  //get task item's id
+  var taskId = event.target.getAttribute("data-task-id");
 
-    //set new values
-    taskSelected.querySelector("h3.task-name").textContent = taskName;
-    taskSelected.querySelector("span.task-type").textContent = taskType;
+  //get teh currently selected option's value and convert to lowercase
+  var statusValue = event.target.value.toLowerCase();
 
-    alert("Task Updated!");
+  //find parent task item based on id
+  var taskSelected = document.querySelector(
+    ".task-item[data-task-id='" + taskId + "']"
+  );
 
-    //reset form and change button back to "Add Task"
-    formEl.removeAttribute("data-task-id");
-    document.querySelector("#save-task").textContent = "Add Task";
+  if (statusValue === "to do") {
+    tasksToDoEl.appendChild(taskSelected);
+  } else if (statusValue === "in progress") {
+    tasksInProgressEl.appendChild(taskSelected);
+  } else if (statusValue === "completed") {
+    tasksCompletedEl.appendChild(taskSelected);
+  }
+};
+
+var completeEditTask = function (taskName, taskType, taskId) {
+  //find matching task list item
+  var taskSelected = document.querySelector(
+    ".task-item[data-task-id='" + taskId + "']"
+  );
+
+  //set new values
+  taskSelected.querySelector("h3.task-name").textContent = taskName;
+  taskSelected.querySelector("span.task-type").textContent = taskType;
+
+  alert("Task Updated!");
+
+  //reset form and change button back to "Add Task"
+  formEl.removeAttribute("data-task-id");
+  document.querySelector("#save-task").textContent = "Add Task";
 };
 
 formEl.addEventListener("submit", taskFormHandler);
 pageContentEl.addEventListener("click", taskButtonHandler);
+pageContentEl.addEventListener("change", taskStatusChangeHandler);
